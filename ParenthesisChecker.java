6a90036423489602f512aa2d1a019d58ed702a8b
Yugam Shukla <42417007+YugamShukla@users.noreply.github.com>
2020-10-15 23:19:09 +0530

    Update ParenthesisChecker.java

--- ParenthesisChecker.java
+++ ParenthesisChecker.java
@@ -2,43 +2,40 @@ package stacks;
 import java.util.Stack;
 
 
-class ParenthesisChecker{
-     public static boolean isBalancedParenthesis(String expn) {
-          Stack<Character> stk = new Stack<Character>();
-          char[] array= expn.toCharArray();
-          for (char ch=0;ch<array.length;ch++) {
-              switch (ch) {
-              case '{':
-              case '[':
-              case '(':
-                  stk.push(ch);
-                  break;
-              case '}':
-                  if (stk.pop() != '{') {
-                      return false;
-                  }
-                  break;
-              case ']':
-                  if (stk.pop() != '[') {
-                      return false;
-                  }
-                  break;
-              case ')':
-                  if (stk.pop() != '(') {
-                      return false;
-                  }
-                  break;
-              }
-          }
-          return stk.isEmpty();
+public class ParenthesesChecker {
+ 
+     public static void main(String[] args) {
+     String checkBalancedExpr1=checkBalancedParentesis("a*(b+c)-(d*e)");
+     System.out.println("a*(b+c)-(d*e) : "+checkBalancedExpr1);
+     String checkBalancedExpr2=checkBalancedParentesis("(a*(b-c)*{d+e}");
+     System.out.println("(a*(b-c)*{d+e} : "+checkBalancedExpr2);
+     }
+     public static String checkBalancedParentesis(String expr)
+     {
+     if (expr.isEmpty())
+     return "Balanced";
+     
+     Stack<Character> stack = new Stack<Character>();
+     for (int i = 0; i < expr.length(); i++)
+     {
+     char current = expr.charAt(i);
+     if (current == '{' || current == '(' || current == '[')
+     {
+     stack.push(current);
+     }
+     if (current == '}' || current == ')' || current == ']')
+     {
+     if (stack.isEmpty())
+     return "Not Balanced";
+     char last = stack.peek();
+     if (current == '}' && last == '{' || current == ')' && last == '(' || current == ']' && last == '[')
+     stack.pop();
+     else 
+     return "Not Balanced";
+     }
+     }
+     return stack.isEmpty()?"Balanced":"Not Balanced";
      }
      
-      public static void main(String[] args) {
-          String expn = "{()}[]";
-          boolean value = isBalancedParenthesis(expn);
-          System.out.println("Given Expn:" + expn);
-          System.out.println("Result after isParenthesisMatched:" + value);
-     }
-
-}
-
+    }
+     
