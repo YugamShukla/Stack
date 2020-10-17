cd44aceb8be77e44e71b0560b977d91195bc21f2
Yugam Shukla <42417007+YugamShukla@users.noreply.github.com>
2020-10-17 17:53:10 +0530

    Update ParenthesisChecker.java

--- ParenthesisChecker.java
+++ ParenthesisChecker.java
@@ -16,26 +16,25 @@ public class ParenthesesChecker {
      return "Balanced";
      
      Stack<Character> stack = new Stack<Character>();
-     for (int i = 0; i < expr.length(); i++)
-     {
-     char current = expr.charAt(i);
-     if (current == '{' || current == '(' || current == '[')
-     {
-     stack.push(current);
-     }
-     if (current == '}' || current == ')' || current == ']')
-     {
-     if (stack.isEmpty())
-     return "Not Balanced";
-     char last = stack.peek();
-     if (current == '}' && last == '{' || current == ')' && last == '(' || current == ']' && last == '[')
-     stack.pop();
-     else 
-     return "Not Balanced";
-     }
+     for (int i = 0; i < expr.length(); i++){
+          char current = expr.charAt(i);
+          if (current == '{' || current == '(' || current == '['){
+               stack.push(current);
+          }
+          if (current == '}' || current == ')' || current == ']'){
+               if (stack.isEmpty()){    
+                    return "Not Balanced";
+               }
+               char top = stack.peek();
+               if (current == '}' && top == '{' || current == ')' && top == '(' || current == ']' && top == '['){
+                    stack.pop();
+               }
+               else {
+                    return "Not Balanced";    
+               } 
+          }
      }
      return stack.isEmpty()?"Balanced":"Not Balanced";
      }
-     
     }
      
